932ea43ac9fb9c6f30c614a08217e1036b509474
unknown <raghurab@BLR2-7200012869.ant.amazon.com>
2015-07-01 16:29:49 +0530

    Version 2.0

--- src/test/java/com/amazon/TestVersion.java
+++ src/test/java/com/amazon/TestVersion.java
@@ -8,7 +8,7 @@ public class TestVersion extends TestCase {
 
 	@Test
 	public void testGetVersion() {
-		String expectedVersion = "1.0";
+		String expectedVersion = "2.0";
 		Version version = new Version();
 		assertTrue("Expected version mismatch", version.getVersion().equalsIgnoreCase(expectedVersion));
 	}
